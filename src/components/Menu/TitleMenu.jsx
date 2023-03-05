a34bee9d883a224c8282e59e0ea1c20516b79324
Yurii <99128333+CrocodileGenna@users.noreply.github.com>
2023-03-05 15:58:13 +0200

    test fix title menu 2

--- src/components/Menu/TitleMenu.jsx
+++ src/components/Menu/TitleMenu.jsx
@@ -3,6 +3,7 @@ import { TitleMenuStyle, OnePosition, Link } from './styled/Menu.styled';
 export function TitleMenu({ props }) {
   //   console.log(props);
   const renderedTypes = new Set();
+  const ret = [];
   return (
     <TitleMenuStyle>
       {props.map(({ title }) => {
@@ -14,7 +15,16 @@ export function TitleMenu({ props }) {
             </OnePosition>
           );
         }
+        return ret;
       })}
     </TitleMenuStyle>
   );
 }
+//  if (!renderedTypes.has(title)) {
+//    renderedTypes.add(title);
+//    return (
+//      <OnePosition key={title}>
+//        <Link href={title}>{title}</Link>
+//      </OnePosition>
+//    );
+//  }
